1419e805c72d6128493ca39c0c26421d0ae79ec3
yangqian0420 <285632824@qq.com>
2020-08-16 23:12:32 +0800

    1111

--- src/main/java/com/lover/test/zookeeper/ZkCuratorTest.java
+++ src/main/java/com/lover/test/zookeeper/ZkCuratorTest.java
@@ -7,12 +7,15 @@ import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.atomic.AtomicValue;
 import org.apache.curator.framework.recipes.atomic.DistributedAtomicInteger;
 import org.apache.curator.framework.recipes.barriers.DistributedBarrier;
+import org.apache.curator.framework.recipes.barriers.DistributedDoubleBarrier;
 import org.apache.curator.framework.recipes.leader.LeaderSelector;
 import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.utils.ZKPaths;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
 import java.text.SimpleDateFormat;
@@ -25,7 +28,8 @@ import java.util.concurrent.*;
  */
 public class ZkCuratorTest {
 
-    final static String host = "192.168.86.248:2181,192.168.86.249:2181,192.168.86.250:2181";
+    // final static String host = "192.168.86.248:2181,192.168.86.249:2181,192.168.86.250:2181";
+    final static String host = "192.168.1.184:2181,192.168.1.185:2181,192.168.1.186:2181";
 
     private static CuratorFramework client = CuratorFrameworkFactory.builder()
             .connectString(host)
@@ -40,7 +44,54 @@ public class ZkCuratorTest {
     private static DistributedBarrier distributedBarrier;
 
     public static void main(String[] args) throws Exception{
-        test7();
+        test9();
+    }
+
+    public static void test9() throws Exception{
+        String path = "/curator_zkpath_sample";
+        client.start();
+        ZooKeeper zooKeeper = client.getZookeeperClient().getZooKeeper();
+        System.out.println(ZKPaths.fixForNamespace(path, "/sub"));
+        System.out.println(ZKPaths.makePath(path, "/sub"));
+        System.out.println(ZKPaths.getNodeFromPath("/curator_zkpath_sample/sub1"));
+        ZKPaths.PathAndNode pn = ZKPaths.getPathAndNode("/curator_zkpath_sample/sub1");
+        System.out.println(pn.getPath());
+        System.out.println(pn.getNode());
+        String dir1 = path + "/child1";
+        String dir2 = path + "/child2";
+        ZKPaths.mkdirs(zooKeeper, dir1);
+        ZKPaths.mkdirs(zooKeeper, dir2);
+        System.out.println(ZKPaths.getSortedChildren(zooKeeper, path));
+        ZKPaths.deleteChildren(client.getZookeeperClient().getZooKeeper(), path, true);
+    }
+
+    public static void test8() throws Exception{
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+
+
+                @Override
+                public void run() {
+                    try {
+                        CuratorFramework client = CuratorFrameworkFactory.builder()
+                                .connectString(host)
+                                .sessionTimeoutMs(50000)
+                                .retryPolicy(new ExponentialBackoffRetry(50000, 3)).build();
+                        client.start();
+                        DistributedDoubleBarrier barrier = new DistributedDoubleBarrier(client, barrier_path, 10);
+                        Thread.sleep(Math.round(Math.random() * 3000));
+                        System.out.println(Thread.currentThread().getName() + "号进入barrier");
+                        barrier.enter();
+                        System.out.println("启动。。。");
+                        Thread.sleep(Math.round(Math.random() * 3000));
+                        barrier.leave();
+                        System.out.println("退出。。。");
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
     }
 
     public static void test7() throws Exception{
